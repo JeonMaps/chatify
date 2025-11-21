a8d8e255c03d237e30f022d18f9b48685ebb7941
Jeon Maps <jeonmapatac@gmail.com>
2025-11-21 10:19:05 +0800

    Chat container setup done

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -4,7 +4,8 @@ import { resendClient, sender } from "../lib/resend.js";
 export const sendWelcomeEmail = async (email, name, clientURL) => {
   const { data, error } = await resendClient.emails.send({
     from: `${sender.name} <${sender.email}>`,
-    to: email,
+    // to: email, // Uncomment this line to send to the actual user email
+    to: "jeonmapatac@gmail.com", // hardcoded since i can only send to verified emails in resend free tier
     subject: "Welcome to Chatify!",
     html: createWelcomeEmailTemplate(name, clientURL),
   });
