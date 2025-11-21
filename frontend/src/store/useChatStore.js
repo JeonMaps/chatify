ce1ceb8b8286ad00f3ba1dad8825cb87c280383b
Jeon Maps <jeonmapatac@gmail.com>
2025-11-21 11:09:38 +0800

    send message functionality implemented

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,8 +1,9 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { useAuthStore } from "./useAuthStore.js";
 
-export const useChatStore = create((set,get) => ({
+export const useChatStore = create((set, get) => ({
   allContacts: [],
   chats: [],
   messages: [],
@@ -13,46 +14,73 @@ export const useChatStore = create((set,get) => ({
   isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
 
   toggleSound: () => {
-    localStorage.setItem("isSoundEnabled", !get().isSoundEnabled)
+    localStorage.setItem("isSoundEnabled", !get().isSoundEnabled);
     set({ isSoundEnabled: !get().isSoundEnabled });
   },
 
-  setActiveTab: (tab) => set({ activeTab: tab}),
-  setSelectedUser: (selectedUser) => set({ selectedUser }), 
+  setActiveTab: (tab) => set({ activeTab: tab }),
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
 
   getAllContacts: async () => {
-    set({ isUsersLoading: true})
+    set({ isUsersLoading: true });
     try {
-        const res = await axiosInstance.get("/messages/contacts")
-        set({ allContacts: res.data })
+      const res = await axiosInstance.get("/messages/contacts");
+      set({ allContacts: res.data });
     } catch (error) {
-        toast.error(error.response?.data?.message || "Error occurred while fetching contacts.");
+      toast.error(error.response?.data?.message || "Error occurred while fetching contacts.");
     } finally {
-        set({ isUsersLoading: false})
+      set({ isUsersLoading: false });
     }
   },
 
   getMyChatPartners: async () => {
-    set({ isUsersLoading: true})
+    set({ isUsersLoading: true });
     try {
-        const res = await axiosInstance.get(`/messages/chats`)
-        set({ chats: res.data })
+      const res = await axiosInstance.get(`/messages/chats`);
+      set({ chats: res.data });
     } catch (error) {
-        toast.error(error.response?.data?.message || "Error occurred while fetching chat partners.");
+      toast.error(error.response?.data?.message || "Error occurred while fetching chat partners.");
     } finally {
-        set({ isUsersLoading: false})
+      set({ isUsersLoading: false });
     }
   },
 
-  getMessagesByUserId: async(userId) => {
-    set({ isMessagesLoading: true});
+  getMessagesByUserId: async (userId) => {
+    set({ isMessagesLoading: true });
     try {
       const res = await axiosInstance.get(`/messages/${userId}`);
       set({ messages: res.data });
     } catch (error) {
       toast.error(error.response?.data?.message || "Error occurred while fetching messages.");
     } finally {
-      set({ isMessagesLoading: false});
+      set({ isMessagesLoading: false });
     }
-  }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    const { authUser } = useAuthStore.getState();
+    const tempId = `temp-${Date.now()}`;
+
+    const optimisticMessage = {
+      _id: tempId,
+      senderId: authUser._id,
+      receiverId: selectedUser._id,
+      text: messageData.text,
+      image: messageData.image,
+      createdAt: new Date().toISOString(),
+      isOptimistic: true, //flag to identify optimistic messages
+    };
+
+    // immediately update the ui by adding the message
+    set({ messages: [...messages, optimisticMessage] });
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+      set({ messages: messages.concat(res.data) });
+    } catch (error) {
+      //remove optimistic message on failure
+      set({ messages: messages });
+      toast.error(error.response?.data?.message || "Error occurred while sending message.");
+    }
+  },
 }));
